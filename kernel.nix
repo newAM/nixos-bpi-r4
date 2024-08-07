02b95408789c2d4bd8579de37540ab6a62616058
Alex Martens <alex@thinglab.org>
2024-08-06 19:54:11 -0700

    kernel: 6.9 -> 6.10
    Official support for 6.9 ended 2024-07-27

--- kernel.nix
+++ kernel.nix
@@ -1,19 +1,19 @@
 {
-  linux_6_9,
+  linux_6_10,
   fetchFromGitHub,
   ...
 }:
-linux_6_9.override {
+linux_6_10.override {
   argsOverride = {
     src = fetchFromGitHub {
       owner = "frank-w";
       repo = "BPI-Router-Linux";
-      # 6.9-main HEAD 2024-08-05
-      rev = "89b23e116975406fddc1610d24d4587495a75e64";
-      hash = "sha256-nyFWd5GGhrk7EdmhpchI9GAsVTuAkdTaRbAUUdisUVo=";
+      # 6.10-main HEAD 2024-08-06
+      rev = "7f8ea2c961d9b931b185d9fa440d904f66d9a786";
+      hash = "sha256-k4ou1blQxtE2KxuM4366ShbH0y4UZ8JqpDcZD9B5JoI=";
     };
-    version = "6.9.0-bpi-r4";
-    modDirVersion = "6.9.0-bpi-r4";
+    version = "6.10.0-bpi-r4";
+    modDirVersion = "6.10.0-bpi-r4";
   };
 
   # https://github.com/frank-w/BPI-Router-Linux/blob/89b23e116975406fddc1610d24d4587495a75e64/arch/arm64/configs/mt7988a_bpi-r4_defconfig
